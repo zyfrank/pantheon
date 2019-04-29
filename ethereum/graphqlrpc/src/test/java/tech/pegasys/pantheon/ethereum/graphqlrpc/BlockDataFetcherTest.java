3043cced5575b588b3031adb22208bc460c02775
zyfrank <zy_frank@hotmail.com>
2019-04-29 11:22:52 +0800

    add and adjust test

---
+++ ethereum/graphqlrpc/src/test/java/tech/pegasys/pantheon/ethereum/graphqlrpc/BlockDataFetcherTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2018 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package tech.pegasys.pantheon.ethereum.graphqlrpc;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+import tech.pegasys.pantheon.ethereum.core.Hash;
+import tech.pegasys.pantheon.util.bytes.BytesValue;
+
+import com.google.common.primitives.UnsignedLong;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BlockDataFetcherTest extends AbstractDataFetcherTest {
+  /*
+   * private DataFetcher<Optional<BlockAdapter>> fetcher;
+   *
+   * @Mock private TransactionPool transactionPool; private GraphQLDataFetchers
+   * fetchers;
+   *
+   * @Mock private Set<Capability> supportedCapabilities;
+   *
+   * @Mock private DataFetchingEnvironment environment;
+   *
+   * @Mock private GraphQLDataFetcherContext context;
+   *
+   * @Mock private BlockchainQuery query;
+   *
+   * @Rule public ExpectedException thrown = ExpectedException.none();
+   *
+   * @Before public void before() { fetchers = new
+   * GraphQLDataFetchers(supportedCapabilities); fetcher =
+   * fetchers.getBlockDataFetcher(); }
+   */
+
+  @Test
+  public void bothNumberAndHashThrows() throws Exception {
+    final Hash fakedHash = Hash.hash(BytesValue.of(1));
+    when(environment.getArgument(eq("number"))).thenReturn(UnsignedLong.valueOf(1));
+    when(environment.getArgument(eq("hash"))).thenReturn(fakedHash);
+
+    thrown.expect(CustomException.class);
+    fetcher.get(environment);
+  }
+
+  @Test
+  public void onlyNumber() throws Exception {
+    // final Hash fakedHash = Hash.hash(BytesValue.of(1));
+    when(environment.getArgument(eq("number"))).thenReturn(UnsignedLong.valueOf(1));
+    when(environment.getArgument(eq("hash"))).thenReturn(null);
+
+    when(environment.getContext()).thenReturn(context);
+    when(context.getBlockchainQuery()).thenReturn(query);
+
+    // thrown.expect(CustomException.class);
+    fetcher.get(environment);
+  }
+}
